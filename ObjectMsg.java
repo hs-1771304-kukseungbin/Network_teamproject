dd790eae22338fd5738df587e5b30fbd1ec7d37c
duse133 <149754945+duse133@users.noreply.github.com>
2023-11-16 14:33:22 +0900

    Add files via upload

--- ObjectMsg.java
+++ ObjectMsg.java
@@ -8,6 +8,7 @@ public class ObjectMsg {
 	ImageIcon Image;
 	long total_user;
 	long total_room;
+	String room_name;
 	
 	public final static int MODE_ID_ERROR = 0x0;
 	public final static int MODE_LOGIN = 0x1;
@@ -18,24 +19,25 @@ public class ObjectMsg {
 	public final static int MODE_TX_FILE = 0x20;
 	public final static int MODE_TX_IMAGE = 0x40;
 	
-	public ObjectMsg(int mode, String id, String message, ImageIcon Image, long size, long room) {
+	public ObjectMsg(int mode, String id, String message, ImageIcon Image, long size, long room, String room_Name) {
 		this.mode = mode;
 		this.Image = Image;
 		this.message = message;
 		this.userName = id;
 		this.total_user = size;
 		this.total_room = room;
+		this.room_name = room_Name;
 	}
 	
 	public ObjectMsg(int mode, String id) {
-		this(mode, id, null, null, 0, 0);
+		this(mode, id, null, null, 0, 0, null);
 	}
 	
 	public ObjectMsg(int mode, String id, String message) {
-		this(mode, id, message, null, 0, 0);
+		this(mode, id, message, null, 0, 0, null);
 	}
 	
 	public ObjectMsg(int mode, String id, String message, ImageIcon Image) {
-		this(mode, id, message, Image, 0, 0);
+		this(mode, id, message, Image, 0, 0, null);
 	}
 }
