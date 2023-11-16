1fc1fba6a31863573231667976ae782176b383b3
duse133 <149754945+duse133@users.noreply.github.com>
2023-11-16 14:22:33 +0900

    Add files via upload

--- ObjectMsg.java
+++ ObjectMsg.java
@@ -3,11 +3,11 @@ import javax.swing.ImageIcon;
 public class ObjectMsg {
 	
 	int mode;
-	String message;
 	String userName;
+	String message;
+	ImageIcon Image;
 	long total_user;
 	long total_room;
-	ImageIcon Image;
 	
 	public final static int MODE_ID_ERROR = 0x0;
 	public final static int MODE_LOGIN = 0x1;
@@ -18,23 +18,24 @@ public class ObjectMsg {
 	public final static int MODE_TX_FILE = 0x20;
 	public final static int MODE_TX_IMAGE = 0x40;
 	
-	public ObjectMsg(int mode, String id, String message, ImageIcon Image, long size) {
+	public ObjectMsg(int mode, String id, String message, ImageIcon Image, long size, long room) {
 		this.mode = mode;
 		this.Image = Image;
 		this.message = message;
 		this.userName = id;
 		this.total_user = size;
+		this.total_room = room;
 	}
 	
 	public ObjectMsg(int mode, String id) {
-		this(mode, id, null, null, 0);
+		this(mode, id, null, null, 0, 0);
 	}
 	
 	public ObjectMsg(int mode, String id, String message) {
-		this(mode, id, message, null, 0);
+		this(mode, id, message, null, 0, 0);
 	}
 	
 	public ObjectMsg(int mode, String id, String message, ImageIcon Image) {
-		this(mode, id, message, Image, 0);
+		this(mode, id, message, Image, 0, 0);
 	}
 }
