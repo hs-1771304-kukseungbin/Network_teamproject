8a93f5eaa14dad21cfc37028080bac03a2f26823
duse133 <149754945+duse133@users.noreply.github.com>
2023-11-21 14:04:31 +0900

    Add files via upload

--- hc_ChatClientRoomGUI.java
+++ hc_ChatClientRoomGUI.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedOutputStream;
@@ -13,14 +15,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 
 public class hc_ChatClientRoomGUI extends hc_ChatClient{
 	
@@ -31,9 +31,10 @@ public class hc_ChatClientRoomGUI extends hc_ChatClient{
 	private ImageIcon i_hanbuk;
 	private JButton b_back;
 	private JButton b_send;
-	private JButton b_sendResource;
+	private JButton b_sendImageIcon;
 	private String title;
 	private DefaultStyledDocument document;
+	private SelectImoticon selectImageGUI;
 	
 
 	public hc_ChatClientRoomGUI(String title) {
@@ -88,7 +89,8 @@ public class hc_ChatClientRoomGUI extends hc_ChatClient{
 				//접속한 방 나가기 모드를 서버에 전송
 				send(new ObjectMsg(ObjectMsg.MODE_OUT_ROOM, mainMenu.userId));
 				roomChat.dispose();
-				roomList.setVisible(true);;
+				if(selectImageGUI != null) selectImageGUI.dispose();
+				roomList.setVisible(true);
 			}
 		});
 		
@@ -103,8 +105,28 @@ public class hc_ChatClientRoomGUI extends hc_ChatClient{
 		JPanel p = new JPanel(new BorderLayout());
 		
 		t_input = new JTextField(30);
+		t_input.addActionListener(new ActionListener () {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message = t_input.getText();
+				if(message.isEmpty()) return;
+				send(new ObjectMsg(ObjectMsg.MODE_TX_STRING, mainMenu.userId, message));
+				t_input.setText("");
+			}
+		});
 		
-		b_sendResource = new JButton("+");
+		b_sendImageIcon = new JButton("@");
+		b_sendImageIcon.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Point location = roomChat.getLocation();
+		        int x = (int) location.getX();
+		        int y = (int) location.getY();
+				ImageIcon image = new ImageIcon();
+				selectImageGUI = new SelectImoticon(x,y);
+				//send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, image));
+			}
+		});
 		
 		b_send = new JButton("보내기");
 		b_send.addActionListener(new ActionListener() {
@@ -117,7 +139,7 @@ public class hc_ChatClientRoomGUI extends hc_ChatClient{
 			}
 		});
 		
-		p.add(b_sendResource, BorderLayout.WEST);
+		p.add(b_sendImageIcon, BorderLayout.WEST);
 		p.add(t_input, BorderLayout.CENTER);
 		p.add(b_send, BorderLayout.EAST);
 		
@@ -150,37 +172,127 @@ public class hc_ChatClientRoomGUI extends hc_ChatClient{
 	}
 	
 	public void printDisplayImage(ObjectMsg msg) {
-		t_display.setCaretPosition(t_display.getDocument().getLength());
-		ImageIcon icon = msg.Image;
-		if(icon.getIconWidth() > 300) {
-			Image img = icon.getImage();
-			Image changeImg = img.getScaledInstance(300, -1, Image.SCALE_SMOOTH);
-			icon = new ImageIcon(changeImg);
-		}
-		
 		document = (DefaultStyledDocument) t_display.getStyledDocument();
 	    int len = document.getLength();
-	    
+	    SimpleAttributeSet attrs = new SimpleAttributeSet();
 	    try {
-	        SimpleAttributeSet attrs = new SimpleAttributeSet();
 	        //자기 자신이 보내는 이모티콘이면 왼쪽 정렬하여 출력
 	        if (mainMenu.userId.equals(msg.userName)) {
 	            StyleConstants.setAlignment(attrs, StyleConstants.ALIGN_LEFT);
 	            t_display.setParagraphAttributes(attrs, true);
+	            document.insertString(len, "\n", null);
+	            t_display.insertIcon(msg.Image);
 	            document.insertString(len, msg.userName + ": " + "\n", null);
-	            t_display.insertIcon(icon);
 	        }
 	        //자기 자신이 아닌 다른 유저가 보낸 이모티콘이면 오른쪽으로 정렬하여 출력
 	        else {
 	            StyleConstants.setAlignment(attrs, StyleConstants.ALIGN_RIGHT);
 	            t_display.setParagraphAttributes(attrs, true);
+	            document.insertString(len, "\n", null);
+	            t_display.insertIcon(msg.Image);
 	            document.insertString(len, " :" + msg.userName + "\n", null);
-	            t_display.insertIcon(icon);
 	        }
-
 	        t_display.setCaretPosition(document.getLength());
 	    } catch (BadLocationException e) {
 	        e.printStackTrace();
 	    }
 	}
+	
+	class SelectImoticon extends JFrame{
+		private int x;
+		private int y;
+		
+		private JButton b_1, b_2, b_3, b_4, b_5, b_6, b_exit;
+		private ImageIcon i_1, i_2, i_3, i_4, i_5, i_6;
+		
+		public SelectImoticon(int x, int y) {
+			this.x = x;
+			this.y = y;
+			setSize(200,280);
+			setLocation(x, y+500);
+			
+			setUndecorated(true);
+			setVisible(true);
+			buildGUI();
+		}
+		
+		private void buildGUI() {
+			add(createSelectImageIcon(),BorderLayout.CENTER);
+			add(createButton(),BorderLayout.SOUTH);
+		}
+		
+		private JPanel createSelectImageIcon() {
+			JPanel p = new JPanel(new GridLayout(3,3));
+			
+			i_1 = new ImageIcon("image/1.png"); b_1 = new JButton(i_1);
+			i_2 = new ImageIcon("image/2.png"); b_2 = new JButton(i_2);
+			i_3 = new ImageIcon("image/3.png"); b_3 = new JButton(i_3);
+			i_4 = new ImageIcon("image/4.png"); b_4 = new JButton(i_4);
+			i_5 = new ImageIcon("image/5.png"); b_5 = new JButton(i_5);
+			i_6 = new ImageIcon("image/6.png"); b_6 = new JButton(i_6);
+			
+			
+			b_1.addActionListener(new ActionListener () {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, i_1));
+				}
+			});
+			
+			b_2.addActionListener(new ActionListener () {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, i_2));
+				}
+			});
+			
+			b_3.addActionListener(new ActionListener () {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, i_3));
+				}
+			});
+			
+			b_4.addActionListener(new ActionListener () {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, i_4));
+				}
+			});
+			
+			b_5.addActionListener(new ActionListener () {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, i_5));
+				}
+			});
+			
+			b_6.addActionListener(new ActionListener () {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					send(new ObjectMsg(ObjectMsg.MODE_TX_IMAGE, mainMenu.userId, null, i_6));
+				}
+			});
+			
+			p.add(b_1); p.add(b_2); p.add(b_3);
+			p.add(b_4); p.add(b_5); p.add(b_6);
+			
+			return p;
+		}
+		
+		
+		private JPanel createButton() {
+			JPanel p = new JPanel(new GridLayout(0,1));
+			b_exit = new JButton("나가기");
+			b_exit.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+			p.add(b_exit);
+			return p;
+		}
+	}
 }
+
